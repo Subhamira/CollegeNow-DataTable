280ca9f77a1a1516142a719e63faf7c86c4c9d5f
Wesley <Wesleymittins@yahoo.com>
2017-09-19 14:29:53 +0000

    no modifacations

--- app.js
+++ app.js
@@ -112,7 +112,6 @@ function createTable(){ //TODO: This should be a function that takes a list of o
 		});                                                       
     },			
 });
-        							
 												
 //function for selected dropdown---------------------------------------------------------------------------------									
 myTable.columns(/*[3,4,5,6,9]*/[3,4,5,6,7,8]).every(function(){
